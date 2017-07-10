ccebb4fb0e28eeb2f238cc4b4ce5c93fab67e2ec
Olivierlu1 <180256@hkis.edu.hk>
2017-07-10 14:05:50 -0500

    Initial Commit

---
+++ FareCompareV2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FareCompareV2
+//
+//  Created by hello on 10/7/2017.
+//  Copyright © 2017 Cipher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
