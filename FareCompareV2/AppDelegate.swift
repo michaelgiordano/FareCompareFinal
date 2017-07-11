865d1f58b21dd9e0b11e5aa0bb95c181d220c4ed
Olivierlu1 <180256@hkis.edu.hk>
2017-07-10 20:54:22 -0500

    added uber api compatible with swift 3

--- FareCompareV2/AppDelegate.swift
+++ FareCompareV2/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UberRides
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // If true, all requests will hit the sandbox, useful for testing
+        Configuration.setSandboxEnabled(true)
+        // If true, Native login will try and fallback to using Authorization Code Grant login (for privileged scopes). Otherwise will redirect to App store
+        Configuration.setFallbackEnabled(false)
+        // Complete other setup
         return true
     }
 
--- FareCompareV2/ViewController.swift
+++ FareCompareV2/ViewController.swift
@@ -7,17 +7,61 @@
 //
 
 import UIKit
+import UberRides
+import CoreLocation
 
 class ViewController: UIViewController {
+    
+    let scopes: [RidesScope] = [.Profile, .Places, .Request]
+    let loginManager = LoginManager(loginType: .native)
+
+    let button = RideRequestButton()
+    let ridesClient = RidesClient()
+    let pickupLocation = CLLocation(latitude: 37.787654, longitude: -122.402760)
+    let dropoffLocation = CLLocation(latitude: 37.775200, longitude: -122.417587)
+    let dropoffNickname = "Work"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let loginButton : LoginButton = LoginButton(frame: CGRect.zero, scopes: scopes, loginManager: loginManager)
+        var builder = RideParametersBuilder().setPickupLocation(pickupLocation).setDropoffLocation(dropoffLocation, nickname: dropoffNickname)
+        
         // Do any additional setup after loading the view, typically from a nib.
+        loginButton.presentingViewController = self
+        loginButton.delegate = self as? LoginButtonDelegate
+        view.addSubview(loginButton)
+        ridesClient.fetchCheapestProduct(pickupLocation: pickupLocation, completion: {
+            product, response in
+            if let productID = product?.productID {
+                builder = builder.setProductID(productID)
+                self.button.rideParameters = builder.build()
+                print(self.button.rideParameters)
+                self.button.loadRideInformation()
+                print(self.button.loadRideInformation())
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // Swift
+
+    
+    // Mark: LoginButtonDelegate
+    
+    public func loginButton(button: LoginButton, didLogoutWithSuccess success: Bool) {
+        // success is true if logout succeeded, false otherwise
+    }
+    
+    public func loginButton(button: LoginButton, didCompleteLoginWithToken accessToken: AccessToken?, error: NSError?) {
+        if let _ = accessToken {
+            // AccessToken Saved
+        } else if let error = error {
+            // An error occured
+        }
     }
 
 
