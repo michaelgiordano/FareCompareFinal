98686642bb4b1b4cb16ce07d925efefbb75f4faa
Olivierlu1 <180256@hkis.edu.hk>
2017-07-12 14:16:26 -0500

    can parse array now

--- FareCompareV2/ViewController.swift
+++ FareCompareV2/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     let ridesClient = RidesClient()
     let button = RideRequestButton()
     
+    var list = [[String: JSON]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //button.delegate = self
@@ -31,84 +33,33 @@ class ViewController: UIViewController {
             "Content-Type" : "application/json"
         ]
         
-           let request = Alamofire.request("https://api.uber.com/v1.2/estimates/time",
-                              parameters: parameters,
-                              encoding: URLEncoding(destination: .queryString),
-                              headers: headers).responseJSON { response in
-            print("Request: \(String(describing: response.request))")   // original url request
-            print("Response: \(String(describing: response.response))") // http url response
-            print("Result: \(response.result)")                         // response serialization result
-        
-         //   if let json = response.result.value {
-
-                var totalEstimates = [Int]()
-                do
-                {
-                    if let theData = response.result.value as? Data,
-                        let json = try JSONSerialization.jsonObject(with: theData) as? [String: Any],
-                        let rides = json["estimate"] as? [[String: Any]]
-                    {
-                        print("theData was deserialized from JSON to Swift")
-                        print("theData: \(theData)")
-                        for ride in rides
-                        {
-                            if let estimate = ride["estimate"] as? Int
-                            {
-                                print("Estimate: \(estimate)")
-                                totalEstimates.append(estimate)
-                            }
-                            else
-                            {
-                                print("ERROR: failed to read estimate")
-                            }
-                        }
-                    }
-                    else
-                    {
-                        print("ERROR: theData was NOT deserialized")
-                    }
-                }
-                catch
-                {
-                    print("Error deserializing JSON: \(error)")
-                }
-                print("totalEstimates: \(totalEstimates)")
-//                print("JSON: \(json)[0]['estimate']") // serialized json response
-//                print(json)
-            }
+        Alamofire.request("https://api.uber.com/v1.2/estimates/time",
+          parameters: parameters,
+          encoding: URLEncoding(destination: .queryString),
+          headers: headers).validate().responseJSON { response in
+            let x = JSON(response.result.value!)
+            print(x["times"]["estimate"])
+            print(x["times"])
+            print(x["times"]["display_name"])
+            self.parse(json: x)
+//            switch response.result {
+//            case .success(let value):
+//                let json = JSON(value)
+//                self.parse(json: json)
+//            case .failure(let error):
+//                print(error)
+//            }
             
-//            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
-//                print("Data: \(utf8Text)") // original server data as UTF8 string
-//            }
-//        
-//        let pickupLocation = CLLocation(latitude: 37.775159, longitude: -122.417907)
-//        let dropoffLocation = CLLocation(latitude: 37.6213129, longitude: -122.3789554)
-//        
-//        //make sure that the pickupLocation and dropoffLocation is set in the Deeplink
-//        let builder = RideParametersBuilder()
-//            .setPickupLocation(pickupLocation)
-//            // nickname or address is required to properly display destination on the Uber App
-//            .setDropoffLocation(dropoffLocation,
-//                                nickname: "San Francisco International Airport")
-//        
-//        // use the same pickupLocation to get the estimate
-//        ridesClient.fetchCheapestProduct(pickupLocation: pickupLocation, completion: {
-//            product, response in
-//            if let productID = product?.productID { //check if the productID exists
-//                builder.setProductID(productID)
-//                self.button.rideParameters = builder.build()
-//                
-//                // show estimate in the button
-//                self.button.loadRideInformation()
-//            }
-//        })
-//        
-//        // center the button (optional)
-//        button.sizeToFit()
-//        button.center = view.center
-//        
-//        //put the button in the view
-//        view.addSubview(button)    }
+        }
+        
+        print(list)
+    }
+    func parse(json: JSON) {
+        for result in json["times"].arrayValue {
+            let estimate = result["estimate"]
+            let displayName = result["display_name"]
+    1
+        }
     }
     
     func rideRequestButtonDidLoadRideInformation(button: RideRequestButton) {
@@ -121,9 +72,9 @@ class ViewController: UIViewController {
     
     // Mark: LoginButtonDelegate
     
- 
-
-
 }
+
+
+
 
 
